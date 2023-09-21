0d140ac544583acb82fa33aa730c3ff53f787eb3
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2023-09-22 01:52:10 +0530

    create the UI scratch and created state management single store and placed positions to put  endpoints

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import logo from "../assets/logo.svg";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="w-full flex justify-between items-center mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/nadeeshaMadusanka99")}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="blue_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with our AI-powered summarizer. <br className="max-md:hidden" />
+        <span className="blue_gradient">Free</span> and{" "}
+        <span className="blue_gradient">Open Source</span>.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
