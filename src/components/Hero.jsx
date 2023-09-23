017839cf7968f3ef5e7f23768550a94fb5c49e47
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2023-09-23 20:01:20 +0530

    added all the functionalities

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
       </nav>
       <h1 className="head_text">
         Summarize Articles with <br className="max-md:hidden" />
-        <span className="blue_gradient">OpenAI GPT-4</span>
+        <span className="blue_gradient">OpenAI GPT</span>
       </h1>
       <h2 className="desc">
         Simplify your reading with our AI-powered summarizer. <br className="max-md:hidden" />
--- src/services/article.js
+++ src/services/article.js
@@ -1,10 +1,25 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
+// const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
 export const articleApi = createApi({
-    reducerPath: 'ArticleApi', 
+    reducerPath: 'articleApi', 
+    baseQuery: fetchBaseQuery({
+        baseUrl : 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set("X-RapidAPI-Key" , '20bf62713amsh806764b8a78176bp13666fjsn7f0eec165715')
+            headers.set("X-RapidAPI-Host" , 'article-extractor-and-summarizer.p.rapidapi.com')
+            return headers;
+        },
+    }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => `test`
-        })
-    })
+            query: (params) => `./summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+        }),
+    }),
 });
+
+export const {useLazyGetSummaryQuery} = articleApi;
+
+// End of my one
+
